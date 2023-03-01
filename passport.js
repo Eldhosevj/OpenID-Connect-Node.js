23446319941957c2aac56d12c180e3d05862e349
eldhosevj <eldhosvj@gmail.com>
2023-03-01 17:16:28 +0530

    change statergy

--- passport.js
+++ passport.js
@@ -1,50 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+var OpenIDConnectStrategy = require('passport-openidconnect');
+
 const { Strategy, discoverAndCreateClient } = require('passport-curity');
 
 // Part 2, configure authentication endpoints
-router.get('/login', passport.authenticate('curity'));
-router.get('/callback', passport.authenticate('curity', { failureRedirect: '/login' }), (req, res) => {
+router.get('/login', passport.authenticate('openidconnect'));
+router.get('/callback', passport.authenticate('openidconnect', { failureRedirect: '/login' }), (req, res) => {
     res.redirect('/user');
 });
 
 // Part 3, configuration of Passport
 const getConfiguredPassport = async () => {
-
-    // Part 3a, discover Curity Server metadata and configure the OIDC client
-    const client = await discoverAndCreateClient({
-        issuerUrl: 'https://sts-lle.atsol.com',
-        clientID: "fstnr.tps",
+    passport.use(new OpenIDConnectStrategy({
+        issuer: 'https://sts-lle.atsol.com',
+        authorizationURL: 'https://sts-lle.atsol.com',
+        tokenURL: 'https://sts-lle.atsol.com',
+        userInfoURL: '',
+        clientID:"fstnr.tps",
         clientSecret: "secret",
-        redirectUris: ["http://localhost:3000/callback"],
-        responseTypes: ["token"]
-    });
+        callbackURL: 'http"//localhost:3000/callback'
+      },()=>{
 
-    // Part 3b, configure the passport strategy
-    const strategy = new Strategy({
-        client,
-        params: {
-            scope: "openid profile email api offline_access"
-        }
-    }, function(accessToken, refreshToken, profile, cb) {
-        return cb(null, { profile });
-    });
-
-    // Part 3c, tell passport to use the strategy
-    passport.use(strategy);
-
-    // Part 3d, tell passport how to serialize and deserialize user data
-    passport.serializeUser((user, done) => {
-        done(null, user);
-    });
-
-    passport.deserializeUser((user, done) => {
-        done(null, user);
-    });
+      }
+     
+    ));
 
     return passport;
 };
+
 
 // Part 4, export objects
 exports = module.exports;
